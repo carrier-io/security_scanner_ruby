b4d681b6e3e1691fc2314a796aee6f8058f995d2
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2023-07-12 18:22:10 +0500

    Adapted to new integration API

--- static/js/integrations.js
+++ static/js/integrations.js
@@ -11,7 +11,7 @@ const rubyIntegration = {
     class="modal modal-small fixed-left fade shadow-sm" tabindex="-1" role="dialog"
 >
     <ModalDialog
-            v-model:description="description"
+            v-model:name="config.name"
             v-model:is_default="is_default"
             @update="update"
             @create="create"
@@ -35,7 +35,7 @@ const rubyIntegration = {
         </template>
         <template #footer>
             <test-connection-button
-                    :apiPath="api_base + 'check_settings'"
+                    :apiPath="this.$root.build_api_url('integrations', 'check_settings') + '/' + pluginName"
                     :error="error.check_connection"
                     :body_data="body_data"
                     v-model:is_fetching="is_fetching"
@@ -55,22 +55,19 @@ const rubyIntegration = {
         })
     },
     computed: {
-        apiPath() {
-            return this.api_base + 'integration/'
-        },
         project_id() {
             return getSelectedProjectId()
         },
         body_data() {
             const {
-                description,
+                config,
                 is_default,
                 project_id,
                 save_intermediates_to,
                 status,
             } = this
             return {
-                description,
+                config,
                 is_default,
                 project_id,
                 save_intermediates_to,
@@ -99,8 +96,8 @@ const rubyIntegration = {
         },
         handleEdit(data) {
             console.debug('ruby editIntegration', data)
-            const {description, is_default, id, settings} = data
-            this.load({...settings, description, is_default, id})
+            const {config, is_default, id, settings} = data
+            this.load({...settings, config, is_default, id})
             this.modal.modal('show')
         },
         handleDelete(id) {
@@ -109,7 +106,7 @@ const rubyIntegration = {
         },
         create() {
             this.is_fetching = true
-            fetch(this.apiPath + this.pluginName, {
+            fetch(this.api_url + this.pluginName, {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(this.body_data)
@@ -139,7 +136,7 @@ const rubyIntegration = {
         },
         update() {
             this.is_fetching = true
-            fetch(this.apiPath + this.id, {
+            fetch(this.api_url + this.id, {
                 method: 'PUT',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(this.body_data)
@@ -155,7 +152,7 @@ const rubyIntegration = {
         },
         delete() {
             this.is_fetching = true
-            fetch(this.apiPath + this.id, {
+            fetch(this.api_url + this.project_id + '/' + this.id, {
                 method: 'DELETE',
             }).then(response => {
                 this.is_fetching = false
@@ -168,8 +165,29 @@ const rubyIntegration = {
                 }
             })
         },
+        async set_default(local) {
+            this.is_fetching = true
+            try {
+                const resp = await fetch(this.api_url + this.project_id + '/' + this.id, {
+                    method: 'PATCH',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({local})
+                })
+                if (resp.ok) {
+                    this.$emit('update', {...this.$data, section_name: this.section_name})
+                } else {
+                    const error_data = await resp.json()
+                    this.handleError(error_data)
+                }
+            } catch (e) {
+                console.error(e)
+                showNotify('ERROR', 'Error setting as default')
+            } finally {
+                this.is_fetching = false
+            }
+        },
         initialState: () => ({
-            description: '',
+            config: {},
             is_default: false,
             is_fetching: false,
             error: {},
@@ -179,7 +197,7 @@ const rubyIntegration = {
             save_intermediates_to: '/data/intermediates/sast',
 
             pluginName: 'security_scanner_ruby',
-            api_base: '/api/v1/integrations/',
+            api_url: V.build_api_url('integrations', 'integration') + '/',
             status: integration_status.success,
         }),
     }
